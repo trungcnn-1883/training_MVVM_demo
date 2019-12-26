033ac8bc777fa71b894f5c9f1dc515a3ea5ad923
ngoc.trung <nguyen.ngoc.trungc@sun-asterisk.com>
2019-12-26 14:16:14 +0700

    first init

---
+++ app/src/main/java/gooner/demo/codelab_mvvm/NewWordActivity.kt
@@ -0,0 +1,12 @@
+package gooner.demo.codelab_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewWordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+    }
+}
